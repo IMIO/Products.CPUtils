48817ce8cec301b004f77c5cc890bf83ac439eef
Stephan Geulette <s.geulette@imio.be>
2008-08-12 19:49:26 +0000

    added external method creation at installation

--- Extensions/AppInstall.py
+++ Extensions/AppInstall.py
@@ -22,96 +22,41 @@
 # 02110-1301, USA.
 #
 
-__author__ = """Gauthier BASTIEN <gbastien@commune.sambreville.be>"""
+__author__ = """Stephan GEULETTE <stephan.geulette@uvcw.be>, Gauthier BASTIEN <gbastien@commune.sambreville.be>"""
 __docformat__ = 'plaintext'
 
 from Products.CMFCore.utils import getToolByName
-from Products.CMFCore.exceptions import BadRequest
 
 from Products.ExternalMethod.ExternalMethod import manage_addExternalMethod
 import logging
 
-logger = logging.getLogger('contacts')
+logger = logging.getLogger('CPUtils')
 
 def install(self, reinstall=False):
     """ 
-     We add the ExternalMethods and the contacts Plone Property Sheet
+        Specific installation steps
     """
-    manage_addExternalMethod(self,
-                             'getEmailAddress',
-                             'getEmailAddress',
-                             'contacts.dict',
-                             'getEmailAddress')
-
-    manage_addExternalMethod(self,
-                             'getContacts',
-                             'getContacts',
-                             'contacts.dict',
-                             'getContacts')
-
-    manage_addExternalMethod(self,
-                             'getSubjectsByContact',
-                             'getSubjectsByContact',
-                             'contacts.dict',
-                             'getSubjectsByContact')
-
-    manage_addExternalMethod(self,
-                             'getLink',
-                             'getLink',
-                             'contacts.dict',
-                             'getLink')
-
-    #we add the Plone Property Sheet to portal_properties
-    #we try/except every property as it could be a new property... during reinstallation
-    try:
-        self.portal_properties.addPropertySheet('contacts_properties', 'contacts Properties', propertysheet=None) 
-    except BadRequest:
-        logger.warn("The contacts_properties sheet already exists")
-    #we add the properties to the Property Sheet
-    try:
-        self.portal_properties.contacts_properties.manage_addProperty('contacts', ('service1', 'service2', 'service3'), 'lines')
-    except BadRequest:
-        logger.warn("The contacts property already exists")
-    try:
-        self.portal_properties.contacts_properties.manage_addProperty('links', ('news', 'events', 'Members'), 'lines')
-    except BadRequest:
-        logger.warn("The links property already exists")
-    try:
-        self.portal_properties.contacts_properties.manage_addProperty('emails', ('email1@test.be', 'email1@test.be|email2@test.be', 'email1@test.be|email2@test.be|email3@test.be'), 'lines')
-    except BadRequest:
-        logger.warn("The emails property already exists")
-    try:
-        self.portal_properties.contacts_properties.manage_addProperty('subjects', ('subject1|subject2', 'subject', 'subject1|subject2|subject3'), 'lines')
-    except BadRequest:
-        logger.warn("The subjects property already exists")
-    try:
-        self.portal_properties.contacts_properties.manage_addProperty('sendToManager', False, 'boolean')
-    except BadRequest:
-        logger.warn("The sendToManager property already exists")
-    try:
-        self.portal_properties.contacts_properties.manage_addProperty('managerEmailAddress', '', 'string')
-    except BadRequest:
-        logger.warn("The managerEmailAddress property already exists")
+    portal = getToolByName(self, 'portal_url').getPortalObject()
+    #Add of some external methods
+    # pack_db method must not be added here
+    for method in ('object_info', ):
+        method_name = 'cputils_'+method
+        if not hasattr(portal.aq_inner.aq_explicit, method_name):
+            logger.info("REALLY Adding external method '%s'" % method_name)
+            #without aq_explicit, if the id exists at a higher level, it is found !
+            manage_addExternalMethod(self, method_name, '', 'CPUtils.utils', method)
     
 def uninstall(self, reinstall=False):
-    """ We remove the contacts Plone Property Sheet """
-    #only remove contacts_properties if we are uninstalling the product
-    #if we reinstall, we want to keep the changes !!!
-    if not reinstall:
-        try:
-            self.portal_properties.manage_delObjects(['contacts_properties'])
-        except BadRequest:
-            #if we do not find the contacts_properties Property Sheet, we pass
-            logger.warn("Unable to delete contacts_properties during uninstall of contacts!")
-            pass
-    
-    #remove the external methods
-    externalMethodsIds = ['getEmailAddress', 'getContacts', 'getSubjectsByContact', 'getLink']
-    
-    #get the portal as External Methods are added at the root of the site
+    """
+        Specific uninstallation steps
+    """
     portal = getToolByName(self, 'portal_url').getPortalObject()
-    for emi in externalMethodsIds:
+    #remove the external methods
+    for method in ('object_info', ):
+        method_name = 'cputils_'+method
         try:
-            portal.manage_delObjects(emi)
+            portal.manage_delObjects(method_name)
+            logger.info("Deleted external method '%s'" % method_name)
         except AttributeError:
-            logger.warn("The '%s' ExternalMethod could not be found, we continue..." % emi)
+            logger.warn("The '%s' ExternalMethod could not be found, we continue..." % method_name)
+
--- Extensions/utils.py
+++ Extensions/utils.py
@@ -27,15 +27,18 @@ def object_info(self):
     """
         return various information on the current object
     """
+    if not check_role(self):
+        return "You must have a manager role to run this script"
     out = []
     from Products.CMFCore.utils import getToolByName
     from Products.CMFCore.WorkflowCore import WorkflowException
     try:
         purl = getToolByName(self, 'portal_url')
-        out.append("portal path='%s'" % purl.getPortalPath())
-        out.append("current object path='%s'" % '/'.join(purl.getRelativeContentPath(self)))
+#        out.append("portal path='%s'" % purl.getPortalPath())
+        out.append("current object path='/%s'" % '/'.join(purl.getRelativeContentPath(self)))
         out.append("current object id='%s'" % self.getId())
         out.append("current object portal type/class='%s'/'%s'" % (self.getPortalTypeName(),self.meta_type))
+        out.append("folderish='%s'" % self.isPrincipiaFolderish)
         wtool = getToolByName(self, 'portal_workflow')
         try:
             workflows = ';'.join([wfw.getId() for wfw in wtool.getWorkflowsFor(self)])
