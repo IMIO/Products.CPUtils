ccba12dabfb47a71b096994d5c76735085b9a31c
Stephan Geulette <s.geulette@imio.be>
2008-08-13 19:35:45 +0000

    log redirected to var/log

--- scripts/pack_db.sh
+++ scripts/pack_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in `cat /root/scripts/INSTANCES.txt`
-do /srv/python244/bin/python /root/scripts/pack_db.py $i >>/root/scripts/pack.log 2>&1
+do /srv/python244/bin/python /root/scripts/pack_db.py $i >>/var/log/pack_db.log 2>&1
 done
