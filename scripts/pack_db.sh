11b322f5de8705799604d58ebe1d6a6142a4518c
Stephan Geulette <s.geulette@imio.be>
2008-08-14 22:04:41 +0000

    corrected erroneous message

--- scripts/pack_db.sh
+++ scripts/pack_db.sh
@@ -3,4 +3,4 @@ echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of pack_db" >>/var/log/pack_db
 for i in `cat /root/scripts/INSTANCES.txt`
 do /srv/python244/bin/python /root/scripts/pack_db.py $i >>/var/log/pack_db.log 2>&1
 done
-echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of pack_db" >>/var/log/pack_db.log
+echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : End of pack_db" >>/var/log/pack_db.log
