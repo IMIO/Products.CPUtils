979df39e835073ba5d26cd032d111319e1549d72
Stephan Geulette <s.geulette@imio.be>
2008-09-03 08:41:29 +0000

    adapted scripts path

--- scripts/pack_backup_db.sh
+++ scripts/pack_backup_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of pack_backup_db"
-for i in `cat /root/scripts/INSTANCES.txt`
-do /srv/python244/bin/python /root/scripts/pack_backup_db.py $i
+for i in `cat /root/cputils_scripts/INSTANCES.txt`
+do /srv/python244/bin/python /root/cputils_scripts/pack_backup_db.py $i
 done
 echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : End of pack_backup_db"
