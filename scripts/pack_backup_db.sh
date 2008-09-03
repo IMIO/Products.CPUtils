f5639ca585e2d66a6c30217c1a038391c2f324ce
Stephan Geulette <s.geulette@imio.be>
2008-09-03 07:21:31 +0000

    adapted comment

--- scripts/pack_backup_db.sh
+++ scripts/pack_backup_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of pack_db"
+echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of pack_backup_db"
 for i in `cat /root/scripts/INSTANCES.txt`
 do /srv/python244/bin/python /root/scripts/pack_backup_db.py $i
 done
-echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : End of pack_db"
+echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : End of pack_backup_db"
