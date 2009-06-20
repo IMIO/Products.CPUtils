53d4cc00193c5c3e6f09d153bfccb5e1442a6094
Stephan Geulette <s.geulette@imio.be>
2009-06-20 19:35:00 +0000

    added role check

--- Extensions/utils.py
+++ Extensions/utils.py
@@ -7,7 +7,7 @@ def check_role(self, role='Manager', context=None):
 def check_zope_admin():
     from AccessControl.SecurityManagement import getSecurityManager
     user = getSecurityManager().getUser()
-    if user.__module__ == 'Products.PluggableAuthService.PropertiedUser':
+    if user.has_role('Manager') and user.__module__ == 'Products.PluggableAuthService.PropertiedUser':
         return True
     return False
 
