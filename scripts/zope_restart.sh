7799ed6f6aa6442c16db7ab103f458efe70bcb3d
Stephan Geulette <s.geulette@imio.be>
2009-12-22 19:32:01 +0000

    added test to run the script always with zope user

--- scripts/zope_restart.sh
+++ scripts/zope_restart.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#if [ "$USER" != "zope" ]
+#then
+#    exec sudo -u zope $0 $@
+#fi
+
 echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of zope_restart"
 for i in `cat /root/scripts/INSTANCES.txt`
 do /srv/python244/bin/python /root/cputils_scripts/zope_restart.py -i $i $*
