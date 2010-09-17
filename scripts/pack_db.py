67f578a30ced73b5a8cc16aab499749d2bfe7bc3
André Nuyens <andre.nuyens@imio.be>
2010-09-17 12:48:24 +0000

    common function moved in utils

--- scripts/pack_db.py
+++ scripts/pack_db.py
@@ -4,75 +4,9 @@
 import sys, urllib, os
 import datetime
 import shutil
+from utils import *
 
-method = 'cputils_pack_db'
-module = 'CPUtils.utils'
-function = 'pack_db'
 buildout_inst_type = None #True for buildout, False for manual instance
-
-def verbose(*messages):
-    print '>>', ' '.join(messages)
-def error(*messages):
-    print >>sys.stderr, '!!', (' '.join(messages))
-
-#------------------------------------------------------------------------------
-
-class MyUrlOpener(urllib.FancyURLopener):
-    def prompt_user_passwd(self, host, realm):
-        return (user,pwd)
-    def __init__(self, *args):
-        self.version = "Zope Packer"
-        urllib.FancyURLopener.__init__(self, *args)
-
-#------------------------------------------------------------------------------
-
-def read_file(zodbfilename, lines):
-    """ read the zope conf filename and include subfile"""
-    try:
-        zfile = open( zodbfilename, 'r')
-    except IOError:
-        error("! Cannot open %s file" % zodbfilename)
-        return
-    for line in zfile.readlines():
-        line = line.strip('\n ')
-        if line.startswith('%include'):
-            otherfilename = line.split()[1]
-            read_file(otherfilename, lines)
-            continue
-        lines.append(line)
-
-#------------------------------------------------------------------------------
-
-def packdb(port, dbs):
-    host = "http://localhost:%s" % port
-    urllib._urlopener = MyUrlOpener()
-    for db in dbs:
-        verbose("\tPacking db '%s' for instance %s (%s days)" % (db, host, days))    
-        url_spd = "%s/Control_Panel/Database/%s/%s?days:float=%s" % (host, db, method, days)
-        #verbose("url='%s'"%url_spd)
-        try:
-            ret_html = urllib.urlopen(url_spd).read()
-            if 'the requested resource does not exist' in ret_html or 'error was encountered while publishing this resource' in ret_html:
-                verbose('\texternal method %s not exist : we will create it'%method)
-                url_em = "%s/manage_addProduct/ExternalMethod/manage_addExternalMethod?id=%s&module=%s&function=%s&title="%(host, method, module, function)
-                try:
-                    ret_html = urllib.urlopen(url_em).read()
-                    if 'the requested resource does not exist' in ret_html or 'error was encountered while publishing this resource' in ret_html:
-                        error('! Cannot create external method in zope')
-                    elif 'The specified module,' in ret_html:
-                        error('! The specified module %s is not present in the instance'%module)
-                    else:
-                        try:
-                            ret_html = urllib.urlopen(url_spd).read()
-#                            if "/Control_Panel/Database/%s"%db not in ret_html:
-#                                error("Problem during compression of %s"%db)
-#                                log.debug(ret_html) 
-                        except IOError, msg:
-                            error("! Cannot open URL %s, aborting : %s" % (url_spd, msg))
-                except Exception, msg:
-                    error("! Cannot open URL %s, aborting : %s" % (url_em, msg))
-        except IOError, msg:
-            error("! Cannot open URL %s, aborting : %s" % (url_spd, msg))
 
 #------------------------------------------------------------------------------
 
@@ -100,7 +34,7 @@ def main():
         zodbfilename = instdir + '/etc/zope.conf'
 
     lines = []
-    read_file(zodbfilename, lines)
+    read_zopeconffile(zodbfilename, lines)
 
     port = ''
     httpflag = False
@@ -124,7 +58,7 @@ def main():
     if not port:
         error("! the port was not found in the config file '%s'"%zodbfilename)
 
-    packdb(port,dbs)
+    packdb(port, dbs, days, 'cputils_pack_db', 'CPUtils.utils', 'pack_db')
     
     #we delete every .old created during packing in the /var directory of the zope instance
     if buildout_inst_type:
