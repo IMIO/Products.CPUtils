c7aa65d712dc6824722c1a74bd935f80d898793b
cparis <cparis>
2011-05-02 13:15:52 +0000

    Ajouter surcharge de validateur

--- __init__.py
+++ __init__.py
@@ -31,6 +31,15 @@ logger.info('Installing Product')
 import os, os.path
 from Globals import package_home
 from Products.CMFCore import utils as cmfutils
+from Acquisition import aq_base
+from DateTime import DateTime
+from ZPublisher.HTTPRequest import FileUpload
+
+from Products.validation.interfaces.IValidator import IValidator
+from Products.validation.i18n import PloneMessageFactory as _
+from Products.validation.i18n import recursiveTranslate
+from Products.validation.i18n import safe_unicode
+from Products.validation.validators.SupplValidators import MaxSizeValidator
 
 try: # New CMF
     from Products.CMFCore import permissions as CMFCorePermissions 
@@ -221,6 +230,52 @@ def listInstalledProducts31(self, showHidden=False):
     res.sort(lambda x,y: cmp(x.get('title', x.get('id', None)),
                              y.get('title', y.get('id', None))))
     return res
+def Toto(self, value, *args, **kwargs):
+        instance = kwargs.get('instance', None)
+        field    = kwargs.get('field', None)
+        site_prop = instance.portal_properties.site_properties
+        
+        # get max size
+#        import pdb; pdb.set_trace()
+        if site_prop.hasProperty('image_maxsize'):
+            maxsize = site_prop.image_maxsize
+        elif kwargs.has_key('maxsize'):
+            maxsize = kwargs.get('maxsize')
+        elif hasattr(aq_base(instance), 'getMaxSizeFor'):
+            maxsize = instance.getMaxSizeFor(field.getName())
+        elif hasattr(field, 'maxsize'):
+            maxsize = field.maxsize
+        else:
+            # set to given default value (default defaults to 0)
+            maxsize = self.maxsize
+
+        if not maxsize:
+            return True
+
+        # calculate size
+        elif (isinstance(value, FileUpload) or isinstance(value, file) or
+              hasattr(aq_base(value), 'tell')):
+            value.seek(0, 2) # eof
+            size = value.tell()
+            value.seek(0)
+        else:
+            try:
+                size = len(value)
+            except TypeError:
+                size = 0
+        size = float(size)
+        sizeMB = (size / (1024 * 1024))
+
+        if sizeMB > maxsize:
+            msg = _("Validation failed($name: Uploaded data is too large: ${size}MB (max ${max}MB)",
+                    mapping = {
+                        'name' : safe_unicode(self.name),
+                        'size' : safe_unicode("%.3f" % sizeMB),
+                        'max' : safe_unicode("%.3f" % maxsize)
+                        })
+            return recursiveTranslate(msg, **kwargs)
+        else:
+            return True
 
 def initialize(context):
     logger.info("MONKEY PATCHING QuickInstallerTool!")
@@ -235,5 +290,7 @@ def initialize(context):
     elif plone_version.startswith('3.'):
         QuickInstallerTool.listInstallableProducts = listInstallableProducts31
         QuickInstallerTool.listInstalledProducts = listInstalledProducts31
+        MaxSizeValidator.__call__ = Toto
 
     logger.info("QuickInstallerTool MONKEY PATCHED FOR PLONE %s!"%plone_version)
+    
