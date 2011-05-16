c32e11fc2d947841c9a40bc0bd463b79d395d808
Stephan Geulette <s.geulette@imio.be>
2011-05-16 07:47:42 +0000

    remove locale log

--- scripts/checkInstances.sh
+++ scripts/checkInstances.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of checkInstances">>zr_instances.log
+echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : Start of checkInstances"
 for i in `cat /root/cputils_scripts/INSTANCES.txt`
 do
 /srv/python244/bin/python /root/cputils_scripts/checkInstances.py -i $i
 done
-echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : End of checkInstances">>zr_instances.log
+echo "## " `date +"%Y-%m-%d, %H:%M:%S"` " : End of checkInstances"
