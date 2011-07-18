7e028a90bc82b2c2baed4c558ac04f998c37e836
Stephan Geulette <s.geulette@imio.be>
2011-07-18 12:44:21 +0000

    added hidden products list for standard sites

--- hiddenProductsList.py
+++ hiddenProductsList.py
@@ -1,9 +1,25 @@
 global dic_hpList
 dic_hpList = {
 'acptheme.cpskin3':[
- 'produit 1',
- 'produit 3',
- 'produit 4'
+  ATVocabularyManager,
+  CacheSetup,
+  CMFSquidTool,
+  Clouseau,
+  CPUtils,
+  ImageEditor,
+  LinguaPlone,
+  Marshall,
+  NuPlone,
+  Ploneboard,
+  RICDescriptive,
+  SimpleAttachment,
+  SmartColorWidget,
+  ZopeRepository,
+  collective.js.jquery,
+  collective.js.jqueryui,
+  plone.app.iterate,
+  plone.app.openid,
+  plone.app.z3cform,
 ],
 'urban':[
  'produit 1',
