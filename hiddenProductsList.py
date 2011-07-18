a8fcc96325dc050e12e03328ac3d007780bb5da7
Simon Delcourt <sdelcourt@imio.be>
2011-07-18 12:37:08 +0000

    added external hidden product list

---
+++ hiddenProductsList.py
@@ -0,0 +1,20 @@
+global dic_hpList
+dic_hpList = {
+'acptheme.cpskin3':[
+ 'produit 1',
+ 'produit 3',
+ 'produit 4'
+],
+'urban':[
+ 'produit 1',
+ 'produit 3',
+ 'produit 4'
+],
+'PloneMeeting:':[
+    
+],
+'TeleServices':[
+    
+]
+}
+
