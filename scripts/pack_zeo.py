95d794d2c2cbb1f30cf23cc53152a86cea878e66
jdumont <jdumont>
2012-04-24 14:35:56 +0000

    New script for the packing of .fs with a zeoserver, modified the python part to make it a bit more generic

--- scripts/pack_zeo.py
+++ scripts/pack_zeo.py
@@ -1,13 +1,27 @@
-import sys
+import sys, os
+
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-c", "--cache", action="store", type="string", dest="cache")
+parser.add_option("-i", "--instance", action="store", type="string", dest="instance")
+parser.add_option("-s", "--storage", action="store", type="string", dest="storage")
+parser.add_option("-d", "--days", action="store", type="string", dest="days")
+parser.add_option("-p", "--port", action="store", type="string", dest="port")
+
+(options, args) = parser.parse_args()
+cache = options.cache
+instance = options.instance
+storage = options.storage
+days = options.days
+port = options.port
+
+                
 sys.path[0:0] = [
-  '/srv/zinstances/cache/eggs/plone.recipe.zope2zeoserver-1.4-py2.4.egg',
-  '/srv/zinstances/cache/eggs/zc.recipe.egg-1.2.2-py2.4.egg',
-  '/srv/zinstances/cache/eggs/setuptools-0.6c11-py2.4.egg',
-  '/srv/zinstances/cache/eggs/zc.buildout-1.4.4-py2.4.egg',
-  '/srv/zinstances/make336-cur/parts/zope2/lib/python',
-  '/srv/zinstances/cache/eggs/plone.recipe.zope2zeoserver-1.4-py2.4.egg',
-  '/srv/zinstances/cache/eggs/zc.buildout-1.4.4-py2.4.egg',
-  '/srv/zinstances/cache/eggs/zc.recipe.egg-1.2.2-py2.4.egg',
+    cache + '/eggs/plone.recipe.zope2zeoserver-1.4-py2.4.egg',
+    cache + '/eggs/zc.recipe.egg-1.2.2-py2.4.egg',
+    cache + '/eggs/setuptools-0.6c11-py2.4.egg',
+    cache + '/eggs/zc.buildout-1.4.4-py2.4.egg',
+    instance + '/parts/zope2/lib/python',
   ]
 
 username = None
@@ -17,18 +31,10 @@ unix = None
 host = "127.0.0.1"
 password = None
 
-from optparse import OptionParser
-parser = OptionParser()
-parser.add_option("-s", "--storage", action="store", type="string", dest="storage")
-parser.add_option("-d", "--days", action="store", type="string", dest="days")
-parser.add_option("-p", "--port", action="store", type="string", dest="port")
 
-(options, args) = parser.parse_args()
-storage = options.storage
-days = options.days
-port = options.port
 
 import plone.recipe.zope2zeoserver.pack
 
 if __name__ == '__main__':
     plone.recipe.zope2zeoserver.pack.main(host, port, unix, days, username, password, realm, blob_dir, storage)
+    
---
+++ scripts/pack_zeo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Script to call the pack of the instance's mount point with 
+# the good parameters 
+#
+# © 2011 The CommunesPlone Project support@communesplone.org
+
+declare -i port
+days=7
+
+for i in $(cat /srv/ZINSTANCES-P| grep ^[/srv] )
+do 
+    directory=$(echo $i | awk -F ";" '{print $1}')
+    port=$(echo $i | awk -F ";" '{print $2}')
+    
+    # If port != 0 then we have a zeoserver
+    if [ $port -ne 0 ]
+    then
+        cache=$(awk '/cache/ {print $1; exit}' "${directory}bin/zeoserver" | xargs dirname | sed s/eggs//)
+        for j in $(ls -1 $directory/var/filestorage/*.fs)
+        do
+            storage=$(basename $j .fs)
+            # Now we have all we need so we can call the python script
+            if [ $storage != "Data" ]
+            then
+                echo "la ligne de Commande sera: ${directory}bin/python /srv/scripts/pack_zeo.py -c $cache -i $directory -s $storage -d 7 -p $port"
+                eval ${directory}bin/python /srv/scripts/pack_zeo.py -c ${cache} -i ${directory} -s ${storage} -d ${days} -p ${port}
+            fi
+        done    
+    fi
+
+done
+
