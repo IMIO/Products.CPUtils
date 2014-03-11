3827e64030415dfbd10b104628d51ef8f153488f
Stephan Geulette <s.geulette@imio.be>
2014-03-11 16:48:51 +0100

    Simplify code.

--- src/Products/CPUtils/utils.py
+++ src/Products/CPUtils/utils.py
@@ -17,27 +17,23 @@ def error(*messages):
 def runCommand(cmd):
     """ run an os command and get back the stdout and stderr outputs """
     os.system(cmd + ' >_cmd_pv.out 2>_cmd_pv.err')
-    stdout = stderr = []
-    try:
-        if os.path.exists('_cmd_pv.out'):
-            ofile = open('_cmd_pv.out', 'r')
-            stdout = ofile.readlines()
-            ofile.close()
-            os.remove('_cmd_pv.out')
-        else:
-            error("File %s does not exist" % '_cmd_pv.out')
-    except IOError:
-        error("Cannot open %s file" % '_cmd_pv.out')
-    try:
-        if os.path.exists('_cmd_pv.err'):
-            ifile = open('_cmd_pv.err', 'r')
-            stderr = ifile.readlines()
-            ifile.close()
-            os.remove('_cmd_pv.err')
-        else:
-            error("File %s does not exist" % '_cmd_pv.err')
-    except IOError:
-        error("Cannot open %s file" % '_cmd_pv.err')
+    stdout = []
+    stderr = []
+
+    def get_output(osfile, result):
+        try:
+            if os.path.exists(osfile):
+                ofile = open(osfile, 'r')
+                result += ofile.readlines()
+                ofile.close()
+                os.remove(osfile)
+            else:
+                error("File %s does not exist" % osfile)
+        except IOError:
+            error("Cannot open %s file" % osfile)
+
+    get_output('_cmd_pv.out', stdout)
+    get_output('_cmd_pv.err', stderr)
     return(stdout, stderr)
 
 #------------------------------------------------------------------------------
