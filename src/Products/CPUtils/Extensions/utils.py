d5b2efae09e1f14c98a5842f3cb2227257a27ae1
boulch <christophe.boulanger@imio.be>
2015-01-15 14:23:08 +0100

    Add new method : cputils_objects_stats

--- src/Products/CPUtils/Extensions/utils.py
+++ src/Products/CPUtils/Extensions/utils.py
@@ -49,7 +49,7 @@ def tobytes(objsize):
         lfsize = float(parts[0])
     except Exception:
         return "First part '%s' of objsize '%s' isn't float" % (parts[0], objsize)
-    if parts[1] == 'kB':
+    if parts[1] == 'KB':
         return lfsize * 1024
     elif parts[1] == 'MB':
         return lfsize * 1024**2
@@ -121,8 +121,8 @@ def install(self):
                    'list_portlets', 'list_context_portlets_by_name', 'copy_image_attribute', 'desactivate_base2dom',
                    'rename_long_ids', 'list_newsletter_users', 'zmi', 'list_used_views', 'list_local_roles',
                    'unlock_webdav_objects', 'reftooltoobjects', 'del_bad_portlet', 'clean_utilities_for',
-                   'clean_provides_for', 'add_subject', 'order_folder', 'move_item', 'export_subscribers_csv',
-                   'move_copy_objects'):
+                   'clean_provides_for', 'add_subject', 'order_folder', 'objects_stats', 'move_item',
+                   'export_subscribers_csv', 'move_copy_objects'):
         method_name = 'cputils_' + method
         if not base_hasattr(self, method_name):
             manage_addExternalMethod(self, method_name, '', 'CPUtils.utils', method)
@@ -2346,6 +2346,81 @@ def unlock_webdav_objects(self, dochange=''):
                 walk_dir(obj)
     walk_dir(portal)
     return '<br />\n'.join(out)
+
+###############################################################################
+
+
+def objects_stats(self):
+    from Products.CMFCore.utils import getToolByName
+    if not check_role(self):
+        return "You must have a manager role to run this script"
+
+    portal_url = getToolByName(self, "portal_url")
+    portal = portal_url.getPortalObject()
+
+    kw = {}
+    kw['path'] = '/'.join(self.getPhysicalPath())
+    results = portal.portal_catalog.searchResults(kw)
+
+    header = "<h1>SEARCH RESULTS</h1> <p>Search in : %s</p> <p>Nb Items found : %d </p>" \
+             % (kw['path'], len(results))
+    out = [header]
+
+    res = []
+    out.append("""counter : %s : %s : %s""" % ('portal_type', 'getObjSize', 'url'))
+    typeTreated = []
+    for r in results:
+        res.append((r.portal_type, r.getObjSize, r.getURL()+'/view', tobytes(r.getObjSize)))
+        isTreated = isTypeTreated(r.portal_type, typeTreated)
+        if isTreated == -1:
+            kw1 = {}
+            kw1['path'] = '/'.join(self.getPhysicalPath())
+            kw1['portal_type'] = r.portal_type
+            resultsForCurrentType = portal.portal_catalog.searchResults(kw1)
+            currValue = tobytes(r.getObjSize)/1024
+            typeTreated.append([r.portal_type, len(resultsForCurrentType), currValue])
+
+        else:
+            currValue = tobytes(r.getObjSize)/1024
+            typeTreated[isTreated][2] = typeTreated[isTreated][2] + currValue
+
+    def sortBySize(row1, row2):
+        size1 = row1[-1]
+        size2 = row2[-1]
+        #reverse order
+        return cmp(size1, size2)
+    res.sort(sortBySize, reverse=True)
+
+    count = 0
+    for row in res:
+        (ptype, size, url, bytes) = row
+        count += 1
+        out.append("""%d : %s : %s : <a href="%s">%s</a>""" % (count, ptype, size, url, url))
+    out.append("<hr/>")
+
+    totalSize = 0
+    for row in typeTreated:
+        (ptype, nb, size) = row
+        totalSize += row[2]
+        out.append("""%s : %d (total => %s KB)""" % (ptype, nb, size))
+    out.append("""<br/>Total size is %s KB""" % (totalSize))
+    out.append("<br />END")
+    return '<br />'.join(out)
+
+###############################################################################
+
+
+# -1 if not find
+# x if find where x is the table place.
+def isTypeTreated(ptype, typeTreated):
+    isTreat = -1
+    tableplace = 0
+    for row in typeTreated:
+        (currtype, nb, size) = row
+        if ptype == currtype:
+            isTreat = tableplace
+        tableplace = tableplace + 1
+    return isTreat
 
 ###############################################################################
 
