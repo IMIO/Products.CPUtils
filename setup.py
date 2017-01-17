2257f467560575e11f23766d00e8bfe02ecec947
Stephan Geulette <s.geulette@imio.be>
2017-01-17 10:54:13 +0100

    Back to development: 1.10

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='Products.CPUtils',
-    version='1.9',
+    version='1.10.dev0',
     description="Some plone utilities as external methods, monkey patches, etc.",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
